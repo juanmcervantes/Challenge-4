a39351b8b8e870ac6539f06de17d42797a647025
juanmcervantes <juancervantes0019@gmail.com>
2023-09-28 14:43:06 -0600

    More questions added to questions.js

--- Assets/js/questions.js
+++ Assets/js/questions.js
@@ -1,9 +1,55 @@
-// Array of question objects
+// list of all questions, choices, and answers
 var questions = [
   {
     title: 'Commonly used data types DO NOT include:',
     choices: ['strings', 'booleans', 'alerts', 'numbers'],
     answer: 'alerts',
   },
-  // Add more questions as needed
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+  {
+    title: 'Which of the following is a JavaScript library for building user interfaces?',
+    choices: ['React', 'Laravel', 'Django', 'Flask'],
+    answer: 'React',
+  },
+  {
+    title: 'Which of the following is true about JavaScript?',
+    choices: [
+      'It is a server-side scripting language',
+      'It is a compiled language',
+      'It cannot interact with the DOM',
+      'It is a client-side scripting language'
+    ],
+    answer: 'It is a client-side scripting language',
+  },
+  {
+    title: 'How do you declare a JavaScript variable?',
+    choices: ['var name;', 'variable name;', 'v name;', 'vari name;'],
+    answer: 'var name;',
+  }
 ];
