d999826727d35fd85c600233af350c1548fb63bb
juanmcervantes <juancervantes0019@gmail.com>
2023-09-28 14:18:57 -0600

    Logic modification 1.2

--- logic.js
+++ logic.js
@@ -6,37 +6,140 @@ var timerId;
 // Variables to reference DOM elements
 var questionsEl = document.getElementById('questions');
 var timerEl = document.getElementById('time');
-// ... other elements ...
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
 
-// Function to start the quiz
+// Sound effects
+var sfxRight = new Audio('assets/sfx/correct.wav');
+var sfxWrong = new Audio('assets/sfx/incorrect.wav');
+
 function startQuiz() {
-  // hide start screen, un-hide questions section, start timer, and get first question
-  // ...
+  // Hide start screen
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
+
+  // Un-hide questions section
+  questionsEl.removeAttribute('class');
+
+  // Start timer
+  timerId = setInterval(clockTick, 1000);
+
+  // Show starting time
+  timerEl.textContent = time;
+
+  getQuestion();
 }
 
-// Function to get the current question
 function getQuestion() {
-  // get current question object from array, update title with current question, clear out any old question choices, loop over choices
-  // ...
+  // Get current question object from array
+  var currentQuestion = questions[currentQuestionIndex];
+
+  // Update title with current question
+  var titleEl = document.getElementById('question-title');
+  titleEl.textContent = currentQuestion.title;
+
+  // Clear out any old question choices
+  choicesEl.innerHTML = '';
+
+  // Loop over choices
+  currentQuestion.choices.forEach(function(choice, i) {
+    // Create new button for each choice
+    var choiceNode = document.createElement('button');
+    choiceNode.setAttribute('class', 'choice');
+    choiceNode.setAttribute('value', choice);
+    choiceNode.textContent = i + 1 + '. ' + choice;
+
+    // Attach click event listener to each choice
+    choiceNode.onclick = questionClick;
+
+    // Display on the page
+    choicesEl.appendChild(choiceNode);
+  });
 }
 
-// Function to handle the answer click
 function questionClick() {
-  // check if user guessed wrong and penalize time, display new time on page, play "wrong" sound effect, flash right/wrong feedback on page for half a second, move to next question
-  // ...
+  // Check if user guessed wrong
+  if (this.value !== questions[currentQuestionIndex].answer) {
+    // Penalize time
+    time -= 15;
+    if (time < 0) {
+      time = 0;
+    }
+    timerEl.textContent = time;
+    sfxWrong.play();
+    feedbackEl.textContent = 'Wrong!';
+  } else {
+    sfxRight.play();
+    feedbackEl.textContent = 'Correct!';
+  }
+
+  // Move to next question
+  currentQuestionIndex++;
+
+  // Check if we've run out of questions or time
+  if (currentQuestionIndex === questions.length || time <= 0) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
 }
 
-// Function to end the quiz
 function quizEnd() {
-  // stop timer, show end screen, show final score, hide questions section
-  // ...
+  // Stop timer
+  clearInterval(timerId);
+
+  // Show end screen
+  var endScreenEl = document.getElementById('end-screen');
+  endScreenEl.removeAttribute('class');
+
+  // Show final score
+  var finalScoreEl = document.getElementById('final-score');
+  finalScoreEl.textContent = time;
+
+  // Hide questions section
+  questionsEl.setAttribute('class', 'hide');
 }
 
-// Function to save the highscore
+function clockTick() {
+  // Update time
+  time--;
+  timerEl.textContent = time;
+
+  // Check if user ran out of time
+  if (time <= 0) {
+    quizEnd();
+  }
+}
+
 function saveHighscore() {
-  // get value of input box, make sure value wasn't empty, get saved scores from localstorage, or if not any, set to empty array, format new score object for current user, save to localstorage, redirect to next page
-  // ...
+  // Get value of input box
+  var initials = initialsEl.value.trim();
+
+  // Make sure value wasn't empty
+  if (initials !== '') {
+    // Get saved scores from local storage, or if not any, set to an empty array
+    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
+
+    // Format new score object for the current user
+    var newScore = {
+      score: time,
+      initials: initials,
+    };
+
+    // Save to local storage
+    highscores.push(newScore);
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
+
+    // Redirect to the next page
+    window.location.href = 'highscores.html';
+  }
 }
 
-// user clicks button to submit initials, start quiz, on answer click, on typing initials
-// ...
+// User clicks button to submit initials
+submitBtn.onclick = saveHighscore;
+
+// User clicks button to start quiz
+startBtn.onclick = startQuiz;
