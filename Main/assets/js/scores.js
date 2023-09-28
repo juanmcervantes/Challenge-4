5fa0409512ad31aade6bc708b60277e9f32b40dd
juanmcervantes <juancervantes0019@gmail.com>
2023-09-28 12:12:15 -0600

    Scores modified v1.0

--- Main/assets/js/scores.js
+++ Main/assets/js/scores.js
@@ -1,30 +1,14 @@
+// Function to print high scores
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
-    var highscores = JSON.parse(window.localStorage.getItem('highscores')) || [];
-  
-    // sort highscores by score property in descending order
-    highscores.sort(function (a, b) {
-      return b.score - a.score;
-    });
-  
-    for (var i = 0; i < highscores.length; i += 1) {
-      // create li tag for each high score
-      var liTag = document.createElement('li');
-      liTag.textContent = highscores[i].initials + ' - ' + highscores[i].score;
-  
-      // display on page
-      var olEl = document.getElementById('highscores');
-      olEl.appendChild(liTag);
-    }
-  }
-  
-  function clearHighscores() {
-    window.localStorage.removeItem('highscores');
-    window.location.reload();
-  }
-  
-  document.getElementById('clear').onclick = clearHighscores;
-  
-  // run function when page loads
-  printHighscores();
-  
+  // get scores from localstorage or set to empty array, sort highscores by score property in descending order, create li tag for each high score and display on page
+  // ...
+}
+
+// Function to clear high scores
+function clearHighscores() {
+  // remove high scores from localstorage and reload the page
+  // ...
+}
+
+// clear high scores on click and run function when page loads
+// ...
